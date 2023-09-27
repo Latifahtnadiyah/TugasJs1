69adce057051b1fe9b7fc81822655fdc6cf38a2b
Latifaaaah <115138970+Latifahtnadiyah@users.noreply.github.com>
2023-09-27 14:16:14 +0700

    Add files via upload

---
+++ tugasjs1.js
@@ -0,0 +1,13 @@
+function getTotalNumber(var1, var2){
+    let result = var1 - var2
+    result
+}
+
+let nama = "latifah tasliyatunadiyah";
+let nim  = 20220040140;
+let nilairpl = 80;
+let nilaiimk = 60;
+let jumlahnilai = ( nilairpl + nilaiimk);
+console.log (`NAMA  : ${nama}`);
+console.log(`NIM : ${nim}`);
+console.log(`JUMLAH NILAI : ${jumlahnilai}`);
